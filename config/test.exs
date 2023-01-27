1ab017eac0e475115b1eb26070fad30e076459dc
leonardobrito <f.leobrito@gmail.com>
2023-01-27 03:07:15 -0300

    :construction_worker: Update CI build removing envs

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :app, App.Repo,
   username: "postgres",
   password: System.get_env("POSTGRES_PASSWORD") || "postgres",
-  hostname: System.get_env("POSTGRES_HOST") || "localhost",
+  hostname: "localhost",
   database: "app_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
